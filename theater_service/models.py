73e84c49a2dbb93bb6b130cc12bca0f470d516b4
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-20 10:51:42 +0200

    Add validation method to Ticket model

--- theater_service/models.py
+++ theater_service/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -11,6 +12,43 @@ class Ticket(models.Model):
     class Meta:
         unique_together = ("row", "seat", "performance",)
         ordering = ["reservation"]
+
+    @staticmethod
+    def validate_ticket(row, seat, theater_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theater_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theater_hall, theater_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theater_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theater_hall,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
         return (f"Row: {self.row}, Seat: {self.seat}. "
