61d6da4d31b51dc9d32474ce8d3835a10f10b98f
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-24 18:04:50 +0200

    Add view sets to views.py

--- theater_service/views.py
+++ theater_service/views.py
@@ -1,3 +1,86 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from theater_service.models import (
+    Ticket,
+    Reservation,
+    Performance,
+    TheaterHall,
+    Play,
+    Actor,
+    Genre
+)
+
+from theater_service.serializers import (
+    TicketSerializer,
+    ReservationSerializer,
+    PerformanceSerializer,
+    TheaterHallSerializer,
+    PlaySerializer,
+    ActorSerializer,
+    GenreSerializer,
+)
+
+
+class TicketViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class ReservationViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class PerformanceViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class TheaterHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = TheaterHall.objects.all()
+    serializer_class = TheaterHallSerializer
+
+
+class PlayViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class ActorViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
