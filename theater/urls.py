0feb8879c2da840c573025ce553e8e5b447323b4
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-30 16:55:29 +0200

    Register user urls in project urls.py

--- theater/urls.py
+++ theater/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/theater/", include("theater_service.urls", namespace="theater"))
+    path("api/theater/", include("theater_service.urls", namespace="theater")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
