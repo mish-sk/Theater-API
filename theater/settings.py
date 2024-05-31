beb34b936b1ca2e5170cf91c3862ba966dd36ebf
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-31 14:48:42 +0200

    Fix typo default permission in settings.py

--- theater/settings.py
+++ theater/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "theatre.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
