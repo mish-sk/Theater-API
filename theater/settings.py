17dbe8ee1870b90aba8e674c5162774cb55a143f
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-31 10:29:55 +0200

    Add default throttle rates to the project

--- theater/settings.py
+++ theater/settings.py
@@ -49,6 +49,12 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "1000/day", "user": "10000/day"},
+
 }
 
 SPECTACULAR_SETTINGS = {
