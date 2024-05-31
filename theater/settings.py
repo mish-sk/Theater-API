1d2f7cc335175b5c57a607a91623712bd9ec25f7
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-05-31 18:06:21 +0200

    Add Dockerfile & .dockerignore

--- theater/settings.py
+++ theater/settings.py
@@ -165,7 +165,7 @@ STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/files/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
